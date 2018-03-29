5d35cb8a4813a1425882fe63d2e6513993ef1991
fer <fernandoza@gmail.com>
2018-03-29 17:00:13 -0500

    Add an example of how jdbc works in a simple java context

---
+++ src/main/java/MySQLDemo.java
@@ -0,0 +1,45 @@
+import java.sql.*;
+import com.mysql.cj.jdbc.Driver;
+
+public class MySQLDemo {
+
+    private Connection connection = null;
+
+    public static void main(String[] args) {
+
+        // 1st step is to connect to the MySQL server
+        try {
+            //  0. Creates the connection
+            DriverManager.registerDriver(new Driver());
+            // 0.1 Replace this values with your own db name and credentials
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/fad_db?serverTimezone=UTC&useSSL=false",
+                    "root",
+                    "codeup"
+            );
+
+            // 2. Create statements
+            Statement stmt = connection.createStatement();
+            // 3. Save the results of the query in a Result set
+            ResultSet rs = stmt.executeQuery("SELECT 1+1 as result");
+            // 4. Move the cursor of the DB to the next (first) result
+            rs.next();
+            // 5. Print the value
+            System.out.println("rs = " + rs.getInt(1));
+
+
+            // More than 1 result at the time
+            ResultSet users = stmt.executeQuery("SELECT * from users");
+            // 5. Print the values
+            while(users.next()){
+                // while.next() Moves the cursor to the next result and tells you if there was a result; (true ,false)
+                System.out.println("users.getString(\"name\") = " + users.getString("name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
