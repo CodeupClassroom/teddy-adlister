8cf4239b8b889bc50bc7ff286af50b82c32e37d2
Zach Gulde <zachgulde@gmail.com>
2018-04-03 08:54:34 -0500

    Start another jdbc example

--- src/main/java/MySQLDemo.java
+++ src/main/java/MySQLDemo.java
@@ -3,43 +3,68 @@ import com.mysql.cj.jdbc.Driver;
 
 public class MySQLDemo {
 
-    private Connection connection = null;
+    private static Connection connection = null;
 
+    public static void simpleExample() throws SQLException {
+        // 2. Create statements
+        Statement stmt = connection.createStatement();
+        // 3. Save the results of the query in a Result set
+        ResultSet rs = stmt.executeQuery("SELECT 1+1 as result");
+        // 4. Move the cursor of the DB to the next (first) result
+        rs.next();
+        // 5. Print the value
+        System.out.println("rs = " + rs.getInt(1));
+
+
+        // More than 1 result at the time
+        ResultSet users = stmt.executeQuery("SELECT * from users");
+        // 5. Print the values
+        while(users.next()){
+            // while.next() Moves the cursor to the next result and tells you if there was a result; (true ,false)
+            System.out.println("users.getString(\"name\") = " + users.getString("name"));
+        }
+    }
+
+    /**
+     * 1. Employees example
+     */
     public static void main(String[] args) {
 
         // 1st step is to connect to the MySQL server
         try {
-            //  0. Creates the connection
             DriverManager.registerDriver(new Driver());
-            // 0.1 Replace this values with your own db name and credentials
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/fad_db?serverTimezone=UTC&useSSL=false",
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/employees?serverTimezone=UTC&useSSL=false",
                     "root",
                     "codeup"
             );
 
-            // 2. Create statements
-            Statement stmt = connection.createStatement();
-            // 3. Save the results of the query in a Result set
-            ResultSet rs = stmt.executeQuery("SELECT 1+1 as result");
-            // 4. Move the cursor of the DB to the next (first) result
-            rs.next();
-            // 5. Print the value
-            System.out.println("rs = " + rs.getInt(1));
+            System.out.println("\n--- simpleExample ---\n");
 
+//            simpleExample();
 
-            // More than 1 result at the time
-            ResultSet users = stmt.executeQuery("SELECT * from users");
-            // 5. Print the values
-            while(users.next()){
-                // while.next() Moves the cursor to the next result and tells you if there was a result; (true ,false)
-                System.out.println("users.getString(\"name\") = " + users.getString("name"));
-            }
+            System.out.println("\n--- employeesExample ---\n");
+
+            employeesExample();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
 
+    }
+
+    private static void employeesExample() throws SQLException {
+
+        Statement stmt = connection.createStatement();
+        System.out.println(stmt);
+
+        ResultSet rs = stmt.executeQuery("SELECT * FROM employees LIMIT 10");
+        System.out.println(rs);
+
+        while( rs.next() ){
+            
+        }
     }
 }
